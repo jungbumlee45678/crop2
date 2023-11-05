c8b84b55b7a93a8f0bb2cf97c4408e1455be62d9
jungbumlee45678 <143382978+jungbumlee45678@users.noreply.github.com>
2023-11-05 22:39:26 +0900

    UT_(11.05) with 코틀린

---
+++ java/com/example/ut/ui/HomeFragment.kt
@@ -0,0 +1,287 @@
+package com.example.ut.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.setPadding
+import androidx.fragment.app.Fragment
+import com.example.ut.server.ApiServer
+import com.example.ut.server.IP
+import com.example.ut.databinding.FragmentHomeBinding
+import com.example.ut.R
+import com.example.ut.board.board_info
+import com.example.ut.a_data.board
+import com.example.ut.alarm.alarm
+import com.example.ut.board.write_page
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar)
+
+        toolbar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.action_menu -> {
+
+                    true
+                }
+                R.id.action_search -> {
+
+                    true
+                }
+                R.id.action_alarm -> {
+                    val intent = Intent(requireContext(),alarm::class.java)
+                    startActivity(intent)
+                    true
+                }
+                // 다른 메뉴 아이템의 처리도 추가할 수 있습니다.
+                else -> false
+            }
+        }
+
+        val IP = IP()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(IP.ip()) // 실제 엔드포인트 URL
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiService =  retrofit.create(ApiServer::class.java)
+
+        val call = apiService.board()
+
+        call.enqueue(object : Callback<board> {
+            override fun onResponse(call: Call<board>, response: Response<board>) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    // 서버 응답 처리
+
+                    if(responseBody!!.items.isNotEmpty()){
+                        val scrollView  = view.findViewById<ScrollView>(R.id.scroll) // 부모 레이아웃 ID로 변경
+                        val linearLayout = scrollView.getChildAt(0) as ViewGroup
+
+                        linearLayout.removeViewAt(0)
+
+                        println("i:${responseBody!!.items.size}")
+                        for(i in 0 until responseBody!!.items.size){
+                            val linearLayout1 = LinearLayout(requireContext())
+                            linearLayout1.orientation = LinearLayout.HORIZONTAL
+                            linearLayout1.setBackgroundResource(R.drawable.text_bar);
+
+                            var layoutParams = LinearLayout.LayoutParams(
+                                LinearLayout.LayoutParams.MATCH_PARENT, // 너비는 부모와 일치
+                                (120 * resources.displayMetrics.density).toInt()// 높이는 내용에 맞게
+                            )
+
+                            layoutParams.topMargin = (10 * resources.displayMetrics.density).toInt()
+                            layoutParams.marginStart = (10 * resources.displayMetrics.density).toInt()
+                            layoutParams.marginEnd = (10 * resources.displayMetrics.density).toInt()
+
+                            linearLayout1.layoutParams = layoutParams
+
+                            linearLayout1.setOnClickListener {
+                                intent(responseBody.items[i].title,
+                                    responseBody.items[i].username,
+                                    responseBody.items[i].num)
+                            }
+                            println(2)
+
+                            val webView = WebView(requireContext())
+                            video(webView,IP,responseBody.items[i].num)
+                            layoutParams = LinearLayout.LayoutParams(
+                                (100 * resources.displayMetrics.density).toInt(), // 너비는 부모와 일치
+                                (100 * resources.displayMetrics.density).toInt()// 높이는 내용에 맞게
+                            )
+
+                            layoutParams.marginStart = (10 * resources.displayMetrics.density).toInt()
+                            layoutParams.gravity = Gravity.CENTER
+                            webView.layoutParams = layoutParams
+
+                            val innerLayout = LinearLayout(requireContext())
+                            layoutParams = LinearLayout.LayoutParams(
+                                LinearLayout.LayoutParams.MATCH_PARENT,
+                                (100 * resources.displayMetrics.density).toInt()
+                            )
+                            layoutParams.marginStart = (10 * resources.displayMetrics.density).toInt()
+                            layoutParams.marginEnd = (10 * resources.displayMetrics.density).toInt()
+                            layoutParams.gravity = Gravity.CENTER
+                            innerLayout.layoutParams = layoutParams
+
+                            innerLayout.orientation = LinearLayout.VERTICAL
+
+                            val titleTextView = TextView(requireContext())
+                            titleTextView.layoutParams = LinearLayout.LayoutParams(
+                                LinearLayout.LayoutParams.MATCH_PARENT,
+                                LinearLayout.LayoutParams.WRAP_CONTENT
+                            )
+                            println(5)
+                            titleTextView.text = responseBody.items[i].title
+                            titleTextView.textSize = 20f
+
+                            val addressTextView = TextView(requireContext())
+                            addressTextView.layoutParams = LinearLayout.LayoutParams(
+                                LinearLayout.LayoutParams.MATCH_PARENT,
+                                LinearLayout.LayoutParams.WRAP_CONTENT
+                            )
+                            println(6)
+                            addressTextView.text = "장소"
+                            addressTextView.textSize = 14f
+
+                            val creditTextView = TextView(requireContext())
+                            creditTextView.layoutParams = LinearLayout.LayoutParams(
+                                LinearLayout.LayoutParams.MATCH_PARENT,
+                                LinearLayout.LayoutParams.WRAP_CONTENT
+                            )
+                            println(7)
+                            creditTextView.text = "가격"
+                            creditTextView.textSize = 20f
+
+                            val viewsTextView = TextView(requireContext())
+                            viewsTextView.layoutParams = LinearLayout.LayoutParams(
+                                LinearLayout.LayoutParams.MATCH_PARENT,
+                                LinearLayout.LayoutParams.MATCH_PARENT
+                            )
+                            println(8)
+                            viewsTextView.gravity = Gravity.END
+                            viewsTextView.text = "조회수"
+                            viewsTextView.textSize = 14f
+
+                            // 부모 레이아웃에 하위 뷰들을 추가
+                            linearLayout1.addView(webView)
+                            linearLayout1.addView(innerLayout)
+
+                            // innerLayout에 하위 뷰들을 추가
+                            innerLayout.addView(titleTextView)
+                            innerLayout.addView(addressTextView)
+                            innerLayout.addView(creditTextView)
+                            innerLayout.addView(viewsTextView)
+
+                            linearLayout.addView(linearLayout1,0)
+                        }
+                    }
+                } else {
+                    // 서버 요청 실패
+                    showAlertDialog("서버 요청에 실패하였습니다.")
+                }
+            }
+
+            override fun onFailure(call: Call<board>, t: Throwable) {
+                // 네트워크 오류 처리
+                showAlertDialog("서버 연결에 실패하였습니다.")
+                println(t.message)
+            }
+        })
+
+        val wirth_button = view.findViewById<Button>(R.id.write)
+        wirth_button.setOnClickListener{
+            val intent = Intent(activity, write_page::class.java)
+            startActivity(intent)
+        }
+
+        return view
+    }
+
+    fun showAlertDialog(text:String) {
+        // AlertDialog 빌더 생성
+        val builder = AlertDialog.Builder(requireContext())
+
+        // 다이얼로그 메시지 설정
+        builder.setMessage(text)
+
+        // 다이얼로그 생성 및 표시
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun intent(title:String, username:String, num:Int){
+        val intent = Intent(requireContext(), board_info::class.java)
+        intent.putExtra("num",num)
+        intent.putExtra("title",title)
+        intent.putExtra("username",username)
+        startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_top, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    private fun video(webView:WebView, adress: IP, num:Int){
+        // 동영상 URL 설정
+        val videoUrl = adress.ip()+"video/"+num
+        val imageUrl = adress.ip()+"image"
+
+        // WebView 설정
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+
+        webView.webViewClient = object: WebViewClient(){
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url!!)
+                return true
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                val javascript = """
+                    javascript:(function() {
+                        var video = document.querySelector('video');
+                        
+                        if (video) {
+                            video.removeAttribute('autoplay');
+                            video.removeAttribute('controls');
+                            video.setAttribute('poster','$imageUrl');
+                            
+                            video.addEventListener('click', function() {
+                                if (video.paused) {
+                                    // 비디오가 일시 정지 상태인 경우, 'play()' 메서드를 호출하여 재생합니다.
+                                    video.play();
+                                } else {
+                                    // 비디오가 재생 중인 경우, 'pause()' 메서드를 호출하여 정지합니다.
+                                    video.pause();
+                                }
+                            });
+                        }
+                    })()
+                """
+
+                webView.evaluateJavascript(javascript, null)
+            }
+        }
+        // 웹 페이지 로드
+        webView.loadUrl(videoUrl)
+    }
+}
---
+++ java/com/example/ut/ui/InformationFragment.kt
@@ -0,0 +1,114 @@
+package com.example.ut.ui
+
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.ut.R
+import com.example.ut.login.LoginActivity
+import com.example.ut.alarm.alarm_keyword
+import com.example.ut.databinding.FragmentDashboardBinding
+import com.example.ut.info.interest_thing
+import com.example.ut.info.my_info
+import com.example.ut.info.purchase_history
+import com.example.ut.info.sale_history
+
+class InformationFragment : Fragment() {
+    private var _binding: FragmentDashboardBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_information, container, false)
+
+        val preferences = requireActivity().getSharedPreferences("session", AppCompatActivity.MODE_PRIVATE)
+        // "session"이라는 이름의 SharedPreferences 파일을 가져옴(없으면 자동으로 생성)
+        val username = preferences.getString("username", "")
+        view.findViewById<TextView>(R.id.Nikname).text = username
+
+        val logout_button = view.findViewById<Button>(R.id.logout)
+        logout_button.setOnClickListener{
+            showAlertDialog("로그아웃하시겠습니까?")
+        }
+
+        val change_info_button = view.findViewById<Button>(R.id.my_info)
+        change_info_button.setOnClickListener{
+            intent(my_info::class.java)
+        }
+
+        val gs_thing_button = view.findViewById<Button>(R.id.gs_thing)
+        gs_thing_button.setOnClickListener{
+            intent(interest_thing::class.java, true)
+        }
+
+        val sell_thing_button = view.findViewById<Button>(R.id.sell_thing)
+        sell_thing_button.setOnClickListener{
+            intent(sale_history::class.java)
+        }
+
+        val buy_thing_button = view.findViewById<Button>(R.id.buy_thing)
+        buy_thing_button.setOnClickListener{
+            intent(purchase_history::class.java)
+        }
+
+        val key_word_button = view.findViewById<Button>(R.id.key_word)
+        key_word_button.setOnClickListener{
+            intent(alarm_keyword::class.java)
+        }
+
+        return view
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun intent(page:Class<*>,bool:Boolean=false){
+        val intent = Intent(requireContext(), page)
+        if(bool){
+            intent.putExtra("start",0)
+        }
+        startActivity(intent)
+    }
+
+    fun showAlertDialog(text:String) {
+        // AlertDialog 빌더 생성
+        val builder = AlertDialog.Builder(requireContext())
+
+        // 다이얼로그 메시지 설정
+        builder.setMessage(text)
+
+        builder.setNegativeButton("확인") { _, _ ->
+            val preferences: SharedPreferences = requireContext().getSharedPreferences("session", AppCompatActivity.MODE_PRIVATE)
+            val editor: SharedPreferences.Editor = preferences.edit()
+            preferences.edit().clear().apply()
+            editor.apply()
+
+            // MainActivity로 이동합니다.
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        builder.setPositiveButton("취소") { _, _ ->
+
+        }
+
+        // 다이얼로그 생성 및 표시
+        val dialog = builder.create()
+        dialog.show()
+    }
+}
